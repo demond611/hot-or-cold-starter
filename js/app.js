711d9a66deaa86d2fd09a9744f445345c9f72227
Robert Walker <demond611@gmail.com>
2015-08-18 19:31:48 -0500

    javascript update

--- js/app.js
+++ js/app.js
@@ -9,15 +9,6 @@ $(function (){
 	    }
 	}
 
-	function getRandomColor() {
-        var letters = '0123456789ABCDEF'.split('');
-        var color = '#';
-        for (var i = 0; i < 6; i++ ) {
-            color += letters[Math.round(Math.random() * 15)];
-        }
-        return color;
-    }
-
     var secretNumber = 0, guessCounter = 0, myGuess = 0;
     var foundNumber = false;
 
@@ -28,21 +19,42 @@ $(function (){
     /* SUBMITS INFO ON FORM SUBMIT */
     $("form").submit(function (event){
     	event.preventDefault();
-    	debug("made it here");
-    	if (foundNumber){
 
+    	myGuess = parseInt($("#userGuess").val());
+
+    	if (myGuess === secretNumber){
+    		$("#feedback").text("YOU GUESSED CORRECTLY!");
+    		// CORRECT SOUND
     	} else {
-    		
+    		// BINARY SEARCH ALGORITHM TO FIND HOW CLOSE
+    		// INCREMENT COUNTER
+    		// ADD GUESSES TO LIST
+    		$("<li>"+myGuess+"</li>").appendTo("#guessList");
+    		// WRONG SOUND
     	}
     });
 
+    function getRandomColor() {
+        var letters = '0123456789ABCDEF'.split('');
+        var color = '#';
+        for (var i = 0; i < 6; i++ ) {
+            color += letters[Math.round(Math.random() * 15)];
+        }
+        return color;
+    }
+
 
 	function NewGame(){
+		// GET NEW SECRET NUMBER
 		secretNumber = GenerateNumber();
+		// SET DEFAULTS
+		$("#feedback").text("Make Your Guess!");
+		$("#userGuess").val("");
+		$("#guessList").empty();
+		// CLEAR GUESSES
+		// CHANGE PANEL COLOR TO INDICATE NEW GAME
+		$("h2").css("background-color", getRandomColor());
 	}
-
-	function FeedBack(){
-    }
 
     function GenerateNumber(){
     	var createNumber = Math.floor( (Math.random() * 100) + 1 );
@@ -51,8 +63,7 @@ $(function (){
     }
 
     $(".new").click(function (){
-  		$("h2").css("background-color", getRandomColor());
-  		NewGame();
+	  	NewGame();
   	});
 
 
